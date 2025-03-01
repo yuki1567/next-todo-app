3d596065736256d2b0a96906d81c9485318c617d
yuki1567 <19961017yuki@gmail.com>
2025-03-01 18:46:54 +0900

    fix: リンクをnextLinkに修正

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,54 +1,56 @@
+import Link from "next/link";
+
 export default function TodoList() {
   return (
     <ul className="list-none">
       <li className="bg-white p-2 flex border-t border-t-gray-300 border-r border-r-gray-300 border-b border-b-gray-300 border-l-4 border-l-blue-500 rounded justify-between my-1">
         <span>タスク1</span>
         <div className="w-36 flex justify-around items-center">
-          <a
+          <Link
             href="#"
             className="text-sm text-blue-800 no-underline cursor-pointer"
           >
             check
-          </a>
+          </Link>
           <span className="text-gray-400">|</span>
-          <a
+          <Link
             href="#"
             className="text-sm text-blue-800 no-underline cursor-pointer"
           >
             edit
-          </a>
+          </Link>
           <span className="text-gray-400">|</span>
-          <a
+          <Link
             href="#"
             className="text-sm text-blue-800 no-underline cursor-pointer"
           >
             delete
-          </a>
+          </Link>
         </div>
       </li>
       <li className="bg-white p-2 flex border-t border-t-gray-300 border-r border-r-gray-300 border-b border-b-gray-300 border-l-4 border-l-blue-500 rounded justify-between my-1">
         <span>タスク2</span>
         <div className="w-36 flex justify-around items-center">
-          <a
+          <Link
             href="#"
             className="text-sm text-blue-700 no-underline cursor-pointer"
           >
             check
-          </a>
+          </Link>
           <span className="text-gray-400">|</span>
-          <a
+          <Link
             href="#"
             className="text-sm text-blue-700 no-underline cursor-pointer"
           >
             edit
-          </a>
+          </Link>
           <span className="text-gray-400">|</span>
-          <a
+          <Link
             href="#"
             className="text-sm text-blue-700 no-underline cursor-pointer"
           >
             delete
-          </a>
+          </Link>
         </div>
       </li>
     </ul>
